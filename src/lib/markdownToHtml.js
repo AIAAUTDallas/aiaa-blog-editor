8333f12ee7006925698f390118d2e7d4501ab97d
Fernando4242 <portillofernando92@gmail.com>
2024-02-25 23:10:30 -0600

    test

--- src/lib/markdownToHtml.js
+++ src/lib/markdownToHtml.js
@@ -15,7 +15,7 @@ import {fromHtmlIsomorphic} from 'hast-util-from-html-isomorphic';
 import {unified} from 'unified';
 import {ElementContent} from 'rehype-highlight/lib';
 
-export default async function markdownToHtml(markdown: string) {
+export default async function markdownToHtml(markdown) {
   const result = await unified()
     .use(remarkParse)
     .use(remarkGfm)
@@ -32,7 +32,7 @@ export default async function markdownToHtml(markdown: string) {
         {
           fragment: true,
         },
-      ).children as Array<ElementContent>,
+      ).children,
     })
     .use(rehypeVideo)
     .use(rehypeHighlight)
